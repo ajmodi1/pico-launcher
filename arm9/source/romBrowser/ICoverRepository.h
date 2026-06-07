2d22b3b3f337779f265745c54a434e881a2be52b
ajmodi1 <70445135+ajmodi1@users.noreply.github.com>
2026-06-07 03:14:21 -0500

    Update ICoverRepository.h

--- arm9/source/romBrowser/ICoverRepository.h
+++ arm9/source/romBrowser/ICoverRepository.h
@@ -12,6 +12,10 @@ public:
     virtual void Initialize() = 0;
     virtual FileCover* GetCoverForFile(
         const FileInfo& fileInfo, const InternalFileInfo* internalFileInfo) const = 0;
+
+    /// @brief Gets the hero image for the given file, or nullptr when none exists.
+    ///        Hero images are looked up by filename in /_pico/heroes/user.
+    virtual FileCover* GetHeroForFile(const FileInfo& fileInfo) const = 0;
 };
 
 inline ICoverRepository::~ICoverRepository() { }

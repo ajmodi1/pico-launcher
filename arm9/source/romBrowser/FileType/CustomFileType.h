f4cfb0b185ee31353c2319105c83a6c2467cb640
ajmodi1 <70445135+ajmodi1@users.noreply.github.com>
2026-06-07 02:11:27 -0500

    Update CustomFileType.h

--- arm9/source/romBrowser/FileType/CustomFileType.h
+++ arm9/source/romBrowser/FileType/CustomFileType.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <stdio.h>
 #include "FileType.h"
 #include "services/settings/FileAssociation.h"
 #include "../Theme/IThemeFileIconFactory.h"
+#include "BmpFileIcon.h"
 
 /// @brief Class representing a custom (user provided) file type.
 class CustomFileType : public FileType
@@ -21,39 +23,55 @@ public:
 
     std::unique_ptr<FileIcon> CreateFileIcon(const TCHAR* fileName,
         const IThemeFileIconFactory* themeFileIconFactory) const override
-    {
+{
+            // Try a custom per-game icon from /_pico/icons/<type>/<fileName>.bmp
+        // (for example /_pico/icons/gbc/Mario Tennis.gbc.bmp)
+        if (fileName && fileName[0] && GetShortName() != nullptr)
+{
+            char path[300];
+            int length = snprintf(path, sizeof(path), "/_pico/icons/%s/%s.bmp", GetShortName(), fileName);
+            if (length > 0 && length < (int)sizeof(path))
+{
+                auto icon = BmpFileIcon::TryCreateFromPath(path);
+                if (icon)
+                                        return icon;
+}
+}
+
         return _baseFileType != nullptr
-            ? _baseFileType->CreateFileIcon(fileName, themeFileIconFactory)
-            : themeFileIconFactory->CreateGenericFileIcon(fileName);
-    }
+                        ? _baseFileType->CreateFileIcon(fileName, themeFileIconFactory)
+                        : (themeFileIconFactory != nullptr
+                            ? themeFileIconFactory->CreateGenericFileIcon(fileName)
+                            : nullptr);
+}
 
     FileCover* CreateFileCover(const TCHAR* fileName) const override
-    {
-        return _baseFileType != nullptr
-            ? _baseFileType->CreateFileCover(fileName)
-            : FileType::CreateFileCover(fileName);
-    };
+{
+            return _baseFileType != nullptr
+                            ? _baseFileType->CreateFileCover(fileName)
+                            : FileType::CreateFileCover(fileName);
+};
 
     bool HasInternalFileInfo() const override
-    {
-        return _baseFileType != nullptr
-            && _baseFileType->HasInternalFileInfo();
-    }
+{
+            return _baseFileType != nullptr
+                            && _baseFileType->HasInternalFileInfo();
+}
 
     InternalFileInfo* CreateInternalFileInfo(const FastFileRef& fastFileRef) const override
-    {
-        return _baseFileType != nullptr
-            ? _baseFileType->CreateInternalFileInfo(fastFileRef)
-            : nullptr;
-    }
+{
+            return _baseFileType != nullptr
+                            ? _baseFileType->CreateInternalFileInfo(fastFileRef)
+                            : nullptr;
+}
 
     bool TrySetLaunchParameters(pload_params_t* launchParameters, const char* filePath) const override
-    {
-        StringUtil::Copy(launchParameters->romPath, _fileAssociation->applicationPath, sizeof(launchParameters->romPath));
+{
+            StringUtil::Copy(launchParameters->romPath, _fileAssociation->applicationPath, sizeof(launchParameters->romPath));
         u32 length = StringUtil::Copy(launchParameters->arguments, filePath, sizeof(launchParameters->arguments));
         launchParameters->argumentsLength = length + 1;
         return true;
-    }
+}
 
 private:
     const FileAssociation* _fileAssociation;

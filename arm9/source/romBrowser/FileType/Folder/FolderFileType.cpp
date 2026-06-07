fd2f88d91a256f4274c4c7477cbef413a1bdae62
ajmodi1 <70445135+ajmodi1@users.noreply.github.com>
2026-06-07 01:02:40 -0500

    Update FolderFileType.cpp

--- arm9/source/romBrowser/FileType/Folder/FolderFileType.cpp
+++ arm9/source/romBrowser/FileType/Folder/FolderFileType.cpp
@@ -1,4 +1,25 @@
 #include "common.h"
+#include <stdio.h>
+#include "../BmpFileIcon.h"
 #include "FolderFileType.h"
 
 const FolderFileType FolderFileType::sInstance;
+
+std::unique_ptr<FileIcon> FolderFileType::CreateFileIcon(const TCHAR* fileName,
+    const IThemeFileIconFactory* themeFileIconFactory) const
+{
+    // Try to load a custom folder icon from /_pico/icons/<folderName>.bmp
+    if (fileName && fileName[0])
+    {
+        char path[280];
+        int length = snprintf(path, sizeof(path), "/_pico/icons/%s.bmp", fileName);
+        if (length > 0 && length < (int)sizeof(path))
+        {
+            auto icon = BmpFileIcon::TryCreateFromPath(path);
+            if (icon)
+                return icon;
+        }
+    }
+
+    return themeFileIconFactory ? themeFileIconFactory->CreateFolderIcon(fileName) : nullptr;
+}

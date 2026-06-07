47ef654bfbae80b8879970c48b8708135da45889
ajmodi1 <70445135+ajmodi1@users.noreply.github.com>
2026-06-07 01:39:41 -0500

    Update SdFolderFactory.cpp

--- arm9/source/romBrowser/SdFolderFactory.cpp
+++ arm9/source/romBrowser/SdFolderFactory.cpp
@@ -3,37 +3,51 @@
 #include "fat/Directory.h"
 #include "FileInfo.h"
 #include "FileType/Folder/FolderFileType.h"
+#include "VBlank.h"
 #include "SdFolderFactory.h"
+
+// Number of directory entries to read before briefly yielding to lower
+// priority threads (such as the UI thread). Without this, enumerating a
+// large folder on the IO thread starves the UI thread and input freezes.
+#define ENTRIES_PER_YIELD   48
 
 std::unique_ptr<SdFolder> SdFolderFactory::CreateFromPath(const char* path) const
 {
-    Directory directory;
+        Directory directory;
     if (directory.Open(path) != FR_OK)
-        return nullptr;
+                return nullptr;
 
     int count = 0;
     int bufferSize = 8;
+    int entriesSinceYield = 0;
     auto fileInfos = (FileInfo**)malloc(sizeof(FileInfo*) * bufferSize);
     auto sdFileInfo = std::make_unique<FILINFO>();
     while (true)
-    {
+{
         if (directory.Read(sdFileInfo.get()) != FR_OK)
-            return nullptr;
+                        return nullptr;
 
         if (sdFileInfo->fname[0] == 0)
-            break;
+                        break;
 
         if (count >= bufferSize)
-        {
+{
             bufferSize *= 2;
             fileInfos = (FileInfo**)realloc(fileInfos, sizeof(FileInfo*) * bufferSize);
-        }
+}
         auto fileType = sdFileInfo->fattrib & AM_DIR
             ? &FolderFileType::sInstance
             : _fileTypeProvider->GetFileType(sdFileInfo->fname);
         fileInfos[count++] = new FileInfo(sdFileInfo->fname, fileType,
-            FastFileRef(directory.GetFatFsDirectory(), sdFileInfo.get()), sdFileInfo->fattrib);
-    }
+                        FastFileRef(directory.GetFatFsDirectory(), sdFileInfo.get()), sdFileInfo->fattrib);
+
+        // Periodically give the UI thread a chance to run so input stays responsive.
+        if (++entriesSinceYield >= ENTRIES_PER_YIELD)
+{
+            entriesSinceYield = 0;
+            VBlank::Wait();
+}
+}
 
     return std::make_unique<SdFolder>(fileInfos, count);
 }

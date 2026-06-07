9b3fefbc41be5154898cbd852e745ca3827e4d3a
ajmodi1 <70445135+ajmodi1@users.noreply.github.com>
2026-06-07 03:17:06 -0500

    Update CoverRepository.h

--- arm9/source/romBrowser/CoverRepository.h
+++ arm9/source/romBrowser/CoverRepository.h
@@ -8,6 +8,7 @@ public:
     void Initialize() override;
     FileCover* GetCoverForFile(
         const FileInfo& fileInfo, const InternalFileInfo* internalFileInfo) const override;
+    FileCover* GetHeroForFile(const FileInfo& fileInfo) const override;
 
 private:
     // Cover folders are loaded lazily on first use, so that the (potentially
@@ -16,10 +17,13 @@ private:
     mutable bool _ndsCoversLoaded = false;
     mutable bool _gbaCoversLoaded = false;
     mutable bool _userCoversLoaded = false;
+    mutable bool _userHeroesLoaded = false;
     mutable std::unique_ptr<SdFolder> _ndsCoversFolder;
     mutable std::unique_ptr<SdFolder> _gbaCoversFolder;
     mutable std::unique_ptr<SdFolder> _userCoversFolder;
+    mutable std::unique_ptr<SdFolder> _userHeroesFolder;
 
     const SdFolder* GetUserCoversFolder() const;
+    const SdFolder* GetUserHeroesFolder() const;
     const SdFolder* GetCoverFolder(const char* coverFolderName) const;
 };
